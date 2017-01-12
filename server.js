68d7bea6fda72c9ab81bae48399ef5b23ee2805f
Patrick Lai <patrick.lai@lifeletters.com.au>
2017-01-12 12:34:26 +1100

    Added server

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 9201;
+
+// Ensure that the JSON objects received from the client get parsed correctly.
+app.use(bodyParser.json())
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function response(req, res) {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(port, '0.0.0.0', function onStart(err) {
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+});
