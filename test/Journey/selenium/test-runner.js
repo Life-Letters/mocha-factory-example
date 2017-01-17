715dbd58bdb1ab6fbd4b5c8cd95407778e43f838
Patrick Lai <patrick.lai@lifeletters.com.au>
2017-01-17 14:34:31 +1100

    Cleaned npm modules

--- test/Journey/selenium/test-runner.js
+++ test/Journey/selenium/test-runner.js
@@ -1,19 +1,16 @@
 require('dotenv').config();
 
-const wdSync = require('wd-sync'),
-      colors = require('colors/safe'),
-      client = wdSync.remote(process.env.SELENIUM_SERVER_REMOTE),
-      fs = require('fs'),
-      path = require('path'),
-      browser = client.browser,
-      sync = client.sync,
+const path = require('path'),
       mocha = require('mocha'),
       MochaFactory = require('mocha-factory');
 
 // Your test files and export path
 const TEST_SCRIPTS_DIR = path.resolve(__dirname);
 const EXPORT_DIR = path.resolve(__dirname, '../export');
-const testList = MochaFactory.convertHtmlFileToJsFile(TEST_SCRIPTS_DIR,EXPORT_DIR);
+
+// Grab all the tests
+const testList = MochaFactory.convertHtmlFileToJsFile(TEST_SCRIPTS_DIR,EXPORT_DIR)
+                 .map((f) => require(f));
 
 describe('Selenium tests - made by IDE', function() {
   it("Runnign IDE tests" , function(done) {
@@ -21,17 +18,10 @@ describe('Selenium tests - made by IDE', function() {
     // Biggest Hack ever - this is actually max it will allow
     this.timeout(999999999);
 
-    sync(function(){
-      browser.init( { browserName: 'phantomjs'} );
-        for(var testfile of testList){
-          const test = require(testfile);
-          console.log(`Test Begin : ${test.name}`);
-          test(browser);
-          console.log(colors.green(`Test Pass✓ : ${test.name}`));
-        }
-      browser.quit();
-      done();
-    });
+    MochaFactory.runIDEtests({
+      selenium_server_remote: process.env.SELENIUM_SERVER_REMOTE,
+      browser_name: 'chrome'
+    }, testList, done);
 
   });
 });
