fdec3cf8069e7093e6b5321bc6224578e18c9a20
Patrick Lai <patrick.lai@lifeletters.com.au>
2017-01-17 11:29:33 +1100

    Updated readme

--- test/Journey/selenium/test-runner.js
+++ test/Journey/selenium/test-runner.js
@@ -1,48 +1,29 @@
 require('dotenv');
 
-const converter = require("selenium-html-js-converter"),
-      path = require('path'),
-      wdSync = require('wd-sync'),
-      fs = require('fs'),
+const wdSync = require('wd-sync'),
       client = wdSync.remote(),
+      path = require('path'),
       browser = client.browser,
       sync = client.sync,
-      mocha = require('mocha');
+      mocha = require('mocha'),
+      MochaFactory = require('mocha-factory');
 
 // Your test files and export path
 const TEST_SCRIPTS_DIR = path.resolve(__dirname);
-const EXPORT_DIR = path.resolve(__dirname, '../Export')
-
-// Convert your tests
-fs.readdir(TEST_SCRIPTS_DIR, (err, files) => {
-  files.forEach(file => {
-    if(file.substr(-5) === '.html'){
-      console.log('converting', file);
-      converter.convertHtmlFileToJsFile(`${TEST_SCRIPTS_DIR}\/${file}`, `${EXPORT_DIR}\/${file.split('.')[0]}.js`);
-    }
-  });
-});
-
-// Your list of tests
-var list_of_tests = new Array();
-
-// Push all the Exported tests into the array
-fs.readdir(EXPORT_DIR, (err, files) => {
-  files.forEach(file => {
-    const test = require(`${EXPORT_DIR}\/${file}`);
-    if(typeof test === 'function') list_of_tests.push(require(`${EXPORT_DIR}\/${file}`));
-  });
-});
-
+const EXPORT_DIR = path.resolve(__dirname, '../export');
+const testList = MochaFactory.convertHtmlFileToJsFile(TEST_SCRIPTS_DIR,EXPORT_DIR);
 
 describe('Selenium tests - made by IDE', function() {
-  sync(function(){
+  it("Tests" , function() {
+
+    sync(function(){
       browser.init( { browserName: 'phantomjs'} );
-      for(var test of list_of_tests){
-        it(test.name , function() {
+        for(var test of testList){
           test(browser);
-        });
-      }
+          console.log(`Test succeeded : ${test.name}`)
+        }
       browser.quit();
+    });
+
   });
 });
